cf981668a17d2080273e8c776b581865453da057
Maria <mariafiroz9@gmail.com>
2025-10-08 23:29:41 +0800

    added static root

--- config/settings.py
+++ config/settings.py
@@ -129,6 +129,8 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, '../moods/build/static')]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

eba2a26d62e2249feef13c091578e23f108b30e8
Maria <mariafiroz9@gmail.com>
2025-10-12 20:54:44 +0800

    Lazy load model to prevent OOM

--- moods_api/classifier.py
+++ moods_api/classifier.py
@@ -1,16 +1,28 @@
-from functools import lru_cache
+# from functools import lru_cache
 
 
-@lru_cache(maxsize=1)
-def model():
-    from transformers import pipeline
-    return pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", framework="tf", return_all_scores=True)
+# @lru_cache(maxsize=1)
+# def model():
+#     from transformers import pipeline
+#     return pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", framework="tf", return_all_scores=True)
+
+# def text_classify(text: str):
+#     # classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
+#     classifier = model();
+#     res = classifier(text)
+    
+#     return res
+
+
+from transformers import pipeline
+
+_classifier = None
 
 def text_classify(text: str):
-    # classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
-    classifier = model();
-    res = classifier(text)
-    
+    global _classifier
+    if _classifier is None:
+       _classifier = pipeline("text-classification",model='bhadresh-savani/distilbert-base-uncased-emotion', return_all_scores=True)
+    res = _classifier(text)
+       
+
     return res
-
-

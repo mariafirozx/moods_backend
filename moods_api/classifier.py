2a12fdf005639f7329a32a254d098c4d45b0ceae
Maria <mariafiroz9@gmail.com>
2025-10-12 19:40:22 +0800

    added cache for the model

--- moods_api/classifier.py
+++ moods_api/classifier.py
@@ -1,8 +1,14 @@
 from transformers import pipeline
+from functools import lru_cache
+
+
+@lru_cache(maxsize=1)
+def model():
+    return pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
 
 def text_classify(text: str):
-    classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
-
+    # classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
+    classifier = model();
     res = classifier(text)
     
     return res

7f528851edcff30748ec6c9cbb2c58619176f987
Maria <mariafiroz9@gmail.com>
2025-10-12 19:53:35 +0800

    applying lazy model loading

--- moods_api/classifier.py
+++ moods_api/classifier.py
@@ -1,9 +1,9 @@
-from transformers import pipeline
 from functools import lru_cache
 
 
 @lru_cache(maxsize=1)
 def model():
+    from transformers import pipeline
     return pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
 
 def text_classify(text: str):

0c32a955a753e8fa3e43c33a01e97e766d09c6b7
Maria <mariafiroz9@gmail.com>
2025-10-15 20:51:26 +0800

    applied hugging face API

--- moods_api/classifier.py
+++ moods_api/classifier.py
@@ -1,5 +1,6 @@
 # from functools import lru_cache
 
+##my original -->>
 
 # @lru_cache(maxsize=1)
 # def model():
@@ -7,22 +8,41 @@
 #     return pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", framework="tf", return_all_scores=True)
 
 # def text_classify(text: str):
-#     # classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", return_all_scores=True)
 #     classifier = model();
 #     res = classifier(text)
     
 #     return res
 
 
-from transformers import pipeline
 
-_classifier = None
 
-def text_classify(text: str):
-    global _classifier
-    if _classifier is None:
-       _classifier = pipeline("text-classification",model='bhadresh-savani/distilbert-base-uncased-emotion', return_all_scores=True, low_cpu_mem_usage=True)
-    res = _classifier(text)
+##using lazy load method -->>
+
+# from transformers import pipeline
+
+# _classifier = None
+
+# def text_classify(text: str):
+#     global _classifier
+#     if _classifier is None:
+#        _classifier = pipeline("text-classification",model='bhadresh-savani/distilbert-base-uncased-emotion', return_all_scores=True, low_cpu_mem_usage=True)
+#     res = _classifier(text)
        
 
-    return res
+#     return res
+
+
+##using hugging face API interference
+
+#hugging face API token -> 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
+
+import os;
+import requests;
+
+API_URL = "https://router.huggingface.co/hf-inference/models/j-hartmann/emotion-english-distilroberta-base"
+headers = {"Authorization": f"Bearer {os.environ['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']}"} 
+
+def text_classify(text:str):
+    res = requests.post(API_URL, headers=headers, json={"inputs": text})
+
+    return res.json()
